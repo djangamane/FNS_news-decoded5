02e22ce38345ce2a3b87bd59f586e6449fa92df9
djangamane <janga.bussaja@gmail.com>
2025-09-15 10:14:50 -0500

    refactor: Use custom script to extract article content

--- backend/src/puppeteerService.js
+++ backend/src/puppeteerService.js
@@ -1,6 +1,4 @@
 const puppeteer = require("puppeteer");
-const { Readability } = require("@mozilla/readability");
-const { JSDOM } = require("jsdom");
 
 async function scrapeArticle(url) {
   let browser = null;
@@ -10,12 +8,8 @@ async function scrapeArticle(url) {
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: "networkidle2" });
 
-    const pageContent = await page.content();
-    const doc = new JSDOM(pageContent, { url });
-    const reader = new Readability(doc.window.document);
-    const article = reader.parse();
-
-    const imageUrl = await page.evaluate(() => {
+    const article = await page.evaluate(() => {
+      const title = document.querySelector("h1")?.innerText;
       const ogImage = document
         .querySelector('meta[property="og:image"]')
         ?.getAttribute("content");
@@ -25,11 +19,28 @@ async function scrapeArticle(url) {
       const twitterImage = document
         .querySelector('meta[name="twitter:image"]')
         ?.getAttribute("content");
-      return ogImage || image || twitterImage || null;
+
+      let bestElement = null;
+      let maxTextLength = 0;
+
+      const elements = document.querySelectorAll("p, div, article");
+      for (const element of elements) {
+        const textLength = element.innerText.length;
+        if (textLength > maxTextLength) {
+          maxTextLength = textLength;
+          bestElement = element.innerText;
+        }
+      }
+
+      return {
+        title,
+        content: bestElement,
+        imageUrl: ogImage || image || twitterImage || null,
+      };
     });
 
     console.log(`Finished scraping article from: ${url}`);
-    return { ...article, imageUrl };
+    return article;
   } catch (error) {
     console.error(`Error scraping article from: ${url}`, error);
     throw error;
